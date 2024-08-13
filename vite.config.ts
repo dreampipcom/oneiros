3d46edca979d361ac8ec8e987776887d6e1f671e
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-13 19:55:28 +0100

    ar(fix) [DPCP-65]: Adding Oneiros to Morpheus

--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,79 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { peerDependencies } from './package.json';
 
-export default defineConfig((env) => ({
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'vite-react-ts-button',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['cjs', 'es'],
-    },
-    rollupOptions: {
-      external: [...Object.keys(peerDependencies), 'react/jsx-runtime'],
-    },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-    react({ fastRefresh: false }),
-    env.mode !== 'test' &&
-      eslint({
-        exclude: ['/virtual:/**', 'node_modules/**', '/sb-preview/**'],
-      }),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.ts',
-  },
-}));
+export default defineConfig((env) => {
+  console.log({ env });
+  return env.MODE == 'es'
+    ? {
+        // es-module
+        build: {
+          lib: {
+            entry: './src/index.ts',
+            name: 'vite-react-ts-button',
+            fileName: (format) => `index.es.js'}`,
+            formats: ['es'],
+          },
+          rollupOptions: {
+            external: [
+              ...Object.keys(peerDependencies),
+              'react/jsx-runtime',
+              'moment',
+            ],
+          },
+          sourcemap: true,
+          emptyOutDir: true,
+        },
+        plugins: [
+          dts({
+            insertTypesEntry: true,
+            tsconfigPath: './ts-config.json',
+          }),
+          react({ fastRefresh: false }),
+          env.mode !== 'test' &&
+            eslint({
+              exclude: ['/virtual:/**', 'node_modules/**', '/sb-preview/**'],
+            }),
+        ],
+        test: {
+          globals: true,
+          environment: 'jsdom',
+          setupFiles: './setupTests.ts',
+        },
+      }
+    : {
+        build: {
+          // cjs
+          lib: {
+            entry: './src/index.ts',
+            name: 'vite-react-ts-button',
+            fileName: (format) => `index.cjs'}`,
+            formats: ['cjs'],
+          },
+          rollupOptions: {
+            external: [
+              ...Object.keys(peerDependencies),
+              'react/jsx-runtime',
+              'moment',
+            ],
+          },
+          sourcemap: true,
+          emptyOutDir: true,
+        },
+        plugins: [
+          dts({
+            insertTypesEntry: true,
+            tsconfigPath: './tsconfig-cjs.json',
+          }),
+          react({ fastRefresh: false }),
+          env.mode !== 'test' &&
+            eslint({
+              exclude: ['/virtual:/**', 'node_modules/**', '/sb-preview/**'],
+            }),
+        ],
+        test: {
+          globals: true,
+          environment: 'jsdom',
+          setupFiles: './setupTests.ts',
+        },
+      };
+});
