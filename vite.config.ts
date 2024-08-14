ca305641f6d2e5622a85c65071ced12102de1ab9
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-14 17:50:17 +0100

    [DPCP-65] Oneiros: Morpheus: Add Oneiros to Morpheus (#15)
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus
    
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus
    
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus
    
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus: let's house keep
    
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus: let's house keep
    
    * ar(fix) [cherry-pick] date range colors
    
    * ar(fix) [cherry-pick] date range colors
    
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus: let's house keep
    
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus: let's house keep
    
    * ar(fix) [DPCP-65]: Adding Oneiros to Morpheus: let's house keep

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,92 @@
 /// <reference types='vitest' />
+import commonjs from 'vite-plugin-commonjs';
 import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { peerDependencies } from './package.json';
 
-export default defineConfig((env) => ({
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'vite-react-ts-button',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['cjs', 'es'],
-    },
-    rollupOptions: {
-      external: [...Object.keys(peerDependencies), 'react/jsx-runtime'],
-    },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-    react({ fastRefresh: false }),
-    env.mode !== 'test' &&
-      eslint({
-        exclude: ['/virtual:/**', 'node_modules/**', '/sb-preview/**'],
-      }),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.ts',
-  },
-}));
+export default defineConfig((env) => {
+  return env.mode != 'cjs'
+    ? {
+        // es-module
+        build: {
+          outDir: './dist/esm',
+          lib: {
+            entry: './src/index.ts',
+            name: 'oneiros-esm',
+            fileName: (format) => `index.es.js`,
+            formats: ['es'],
+          },
+          rollupOptions: {
+            external: [
+              ...Object.keys(peerDependencies),
+              'react/jsx-runtime',
+              'moment',
+            ],
+          },
+          sourcemap: true,
+          emptyOutDir: false,
+        },
+        plugins: [
+          dts({
+            insertTypesEntry: true,
+            tsconfigPath: './tsconfig.json',
+          }),
+          react({ fastRefresh: false }),
+          env.mode !== 'test' &&
+            eslint({
+              exclude: ['/virtual:/**', 'node_modules/**', '/sb-preview/**'],
+            }),
+        ],
+        test: {
+          globals: true,
+          environment: 'jsdom',
+          setupFiles: './setupTests.ts',
+        },
+      }
+    : {
+        build: {
+          // cjs
+          outDir: './dist/cjs',
+          lib: {
+            entry: './src/index.ts',
+            name: 'oneiros-cjs',
+            fileName: (format) => `index.cjs`,
+            formats: ['cjs'],
+          },
+          rollupOptions: {
+            external: [
+              ...Object.keys(peerDependencies),
+              'react/jsx-runtime',
+              'moment',
+            ],
+            onwarn(warning, defaultHandler) {
+              if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+                return;
+              }
+              defaultHandler(warning);
+            },
+          },
+          sourcemap: true,
+          emptyOutDir: false,
+        },
+        plugins: [
+          dts({
+            insertTypesEntry: true,
+            tsconfigPath: './tsconfig-cjs.json',
+          }),
+          commonjs(),
+          // react({ fastRefresh: false }),
+          env.mode !== 'test' &&
+            eslint({
+              exclude: ['/virtual:/**', 'node_modules/**', '/sb-preview/**'],
+            }),
+        ],
+        test: {
+          globals: true,
+          environment: 'jsdom',
+          setupFiles: './setupTests.ts',
+        },
+      };
+});
