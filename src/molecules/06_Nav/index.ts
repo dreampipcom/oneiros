6251b8c76dda0512b09c50ab358a953ebd875e61
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-05-11 22:15:42 -0300

    ar(fix) add exports

--- src/molecules/06_Nav/index.ts
+++ src/molecules/06_Nav/index.ts
@@ -1,3 +1,3 @@
 // @atoms/Nav/index.ts
-export { default as Nav, ENavVariant, ESpotBadgeVariant } from './Nav';
+export { default as Nav, ENavVariant, ENavControlVariant, ENavItemVariant, ESpotBadgeVariant } from './Nav';
 export const NavName = 'Nav';
