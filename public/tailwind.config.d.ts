7a598613ed5b12216db37eab5c42f9931f902b68
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2025-05-11 22:19:12 -0300

    ar(lint)

--- public/tailwind.config.d.ts
+++ public/tailwind.config.d.ts
@@ -1,60 +1,184 @@
 export declare const DreamPipColors: {
-    transparent: string;
-    logo: {
+  transparent: string;
+  logo: {
+    light: {
+      st0: string;
+      st1: string;
+      st2: string;
+      st3: string;
+      st4: string;
+      st5: string;
+      st6: string;
+      st7: string;
+    };
+    dark: {
+      st0: string;
+      st1: string;
+      st2: string;
+      st3: string;
+      st4: string;
+      st5: string;
+      st6: string;
+      st7: string;
+    };
+    darker: {
+      st0: string;
+      st1: string;
+      st2: string;
+      st3: string;
+      st4: string;
+      st5: string;
+      st6: string;
+      st7: string;
+    };
+  };
+  card: {
+    white: string;
+    soft: string;
+    dark: string;
+    foggy: string;
+  };
+  icon: {
+    light: {
+      primary: string;
+      secondary: string;
+      bw: string;
+      white: string;
+      bg: string;
+    };
+    dark: {
+      primary: string;
+      secondary: string;
+      bw: string;
+      white: string;
+      bg: string;
+    };
+  };
+  primary: {
+    light: string;
+    contrast: string;
+    dark: string;
+    dark2: string;
+    passion: string;
+    passionLight: string;
+    passionSoft: string;
+    soft: string;
+    green: string;
+    purpleHaze: string;
+    white: string;
+  };
+  secondary: {
+    light: string;
+    constrast: string;
+    dark: string;
+    passion: string;
+    passionLight: string;
+    passionSoft: string;
+    soft: string;
+    green: string;
+    purpleHaze: string;
+    white: string;
+  };
+  gradient: {
+    soft: {
+      light: {
+        from: string;
+        to: string;
+      };
+      dark: {
+        from: string;
+        to: string;
+      };
+    };
+  };
+  soft: {
+    light: string;
+    constrast: string;
+    bg: string;
+    dark: string;
+  };
+  outro: {
+    light: string;
+    dark: string;
+  };
+  body: {
+    primary: string;
+    light: string;
+    dark: string;
+    soft: string;
+    passion: string;
+  };
+  inverse: {
+    light: string;
+    dark: string;
+  };
+  neutral: {
+    light: string;
+    dark: string;
+  };
+};
+declare const _default: {
+  content: string[];
+  important: string;
+  safelist: string[];
+  theme: {
+    colors: {
+      transparent: string;
+      logo: {
         light: {
-            st0: string;
-            st1: string;
-            st2: string;
-            st3: string;
-            st4: string;
-            st5: string;
-            st6: string;
-            st7: string;
+          st0: string;
+          st1: string;
+          st2: string;
+          st3: string;
+          st4: string;
+          st5: string;
+          st6: string;
+          st7: string;
         };
         dark: {
-            st0: string;
-            st1: string;
-            st2: string;
-            st3: string;
-            st4: string;
-            st5: string;
-            st6: string;
-            st7: string;
+          st0: string;
+          st1: string;
+          st2: string;
+          st3: string;
+          st4: string;
+          st5: string;
+          st6: string;
+          st7: string;
         };
         darker: {
-            st0: string;
-            st1: string;
-            st2: string;
-            st3: string;
-            st4: string;
-            st5: string;
-            st6: string;
-            st7: string;
+          st0: string;
+          st1: string;
+          st2: string;
+          st3: string;
+          st4: string;
+          st5: string;
+          st6: string;
+          st7: string;
         };
-    };
-    card: {
+      };
+      card: {
         white: string;
         soft: string;
         dark: string;
         foggy: string;
-    };
-    icon: {
+      };
+      icon: {
         light: {
-            primary: string;
-            secondary: string;
-            bw: string;
-            white: string;
-            bg: string;
+          primary: string;
+          secondary: string;
+          bw: string;
+          white: string;
+          bg: string;
         };
         dark: {
-            primary: string;
-            secondary: string;
-            bw: string;
-            white: string;
-            bg: string;
+          primary: string;
+          secondary: string;
+          bw: string;
+          white: string;
+          bg: string;
         };
-    };
-    primary: {
+      };
+      primary: {
         light: string;
         contrast: string;
         dark: string;
@@ -66,8 +190,8 @@ export declare const DreamPipColors: {
         green: string;
         purpleHaze: string;
         white: string;
-    };
-    secondary: {
+      };
+      secondary: {
         light: string;
         constrast: string;
         dark: string;
@@ -78,333 +202,209 @@ export declare const DreamPipColors: {
         green: string;
         purpleHaze: string;
         white: string;
-    };
-    gradient: {
+      };
+      gradient: {
         soft: {
-            light: {
-                from: string;
-                to: string;
-            };
-            dark: {
-                from: string;
-                to: string;
-            };
+          light: {
+            from: string;
+            to: string;
+          };
+          dark: {
+            from: string;
+            to: string;
+          };
         };
-    };
-    soft: {
+      };
+      soft: {
         light: string;
         constrast: string;
         bg: string;
         dark: string;
-    };
-    outro: {
+      };
+      outro: {
         light: string;
         dark: string;
-    };
-    body: {
+      };
+      body: {
         primary: string;
         light: string;
         dark: string;
         soft: string;
         passion: string;
-    };
-    inverse: {
+      };
+      inverse: {
         light: string;
         dark: string;
-    };
-    neutral: {
+      };
+      neutral: {
         light: string;
         dark: string;
+      };
     };
-};
-declare const _default: {
-    content: string[];
-    important: string;
-    safelist: string[];
-    theme: {
-        colors: {
-            transparent: string;
-            logo: {
-                light: {
-                    st0: string;
-                    st1: string;
-                    st2: string;
-                    st3: string;
-                    st4: string;
-                    st5: string;
-                    st6: string;
-                    st7: string;
-                };
-                dark: {
-                    st0: string;
-                    st1: string;
-                    st2: string;
-                    st3: string;
-                    st4: string;
-                    st5: string;
-                    st6: string;
-                    st7: string;
-                };
-                darker: {
-                    st0: string;
-                    st1: string;
-                    st2: string;
-                    st3: string;
-                    st4: string;
-                    st5: string;
-                    st6: string;
-                    st7: string;
-                };
-            };
-            card: {
-                white: string;
-                soft: string;
-                dark: string;
-                foggy: string;
-            };
-            icon: {
-                light: {
-                    primary: string;
-                    secondary: string;
-                    bw: string;
-                    white: string;
-                    bg: string;
-                };
-                dark: {
-                    primary: string;
-                    secondary: string;
-                    bw: string;
-                    white: string;
-                    bg: string;
-                };
-            };
-            primary: {
-                light: string;
-                contrast: string;
-                dark: string;
-                dark2: string;
-                passion: string;
-                passionLight: string;
-                passionSoft: string;
-                soft: string;
-                green: string;
-                purpleHaze: string;
-                white: string;
-            };
-            secondary: {
-                light: string;
-                constrast: string;
-                dark: string;
-                passion: string;
-                passionLight: string;
-                passionSoft: string;
-                soft: string;
-                green: string;
-                purpleHaze: string;
-                white: string;
-            };
-            gradient: {
-                soft: {
-                    light: {
-                        from: string;
-                        to: string;
-                    };
-                    dark: {
-                        from: string;
-                        to: string;
-                    };
-                };
-            };
-            soft: {
-                light: string;
-                constrast: string;
-                bg: string;
-                dark: string;
-            };
-            outro: {
-                light: string;
-                dark: string;
-            };
-            body: {
-                primary: string;
-                light: string;
-                dark: string;
-                soft: string;
-                passion: string;
-            };
-            inverse: {
-                light: string;
-                dark: string;
-            };
-            neutral: {
-                light: string;
-                dark: string;
-            };
-        };
-        fontSize: {
-            sm: string;
-            md: string;
-            base: string;
-            xl: string;
-            '1xl': string;
-            '2xl': string;
-            '3xl': string;
-            '4xl': string;
-            '5xl': string;
-        };
-        letterSpacing: {
-            a0: string;
-            a1: string;
-            a2: string;
-            a3: string;
-            a4: string;
-            a5: string;
-            a6: string;
-            b0: string;
-            b1: string;
-            b2: string;
-            b3: string;
-            b4: string;
-            b5: string;
-            b6: string;
-        };
-        lineHeight: {
-            a0: string;
-            a1: string;
-            a2: string;
-            a3: string;
-            a4: string;
-            a5: string;
-            a6: string;
-            b0: string;
-            b1: string;
-            b2: string;
-            b3: string;
-            b4: string;
-            b5: string;
-            b6: string;
-        };
-        spacing: {
-            0: string;
-            a0: string;
-            a1: string;
-            a2: string;
-            a3: string;
-            a4: string;
-            a5: string;
-            a6: string;
-            a7: string;
-            a8: string;
-            a9: string;
-            a10: string;
-            b0: string;
-            b1: string;
-            b2: string;
-            b3: string;
-            b4: string;
-            b5: string;
-            b6: string;
-        };
-        sizing: {
-            0: string;
-            a0: string;
-            a1: string;
-            a2: string;
-            a3: string;
-            a4: string;
-            a5: string;
-            a6: string;
-            a7: string;
-            a8: string;
-            a9: string;
-            a10: string;
-            b0: string;
-            b1: string;
-            b2: string;
-            b3: string;
-            b4: string;
-            b5: string;
-            b6: string;
-        };
-        aspectRatio: {
-            square: string;
-            blog: string;
-            tv: string;
-            cinema: string;
-        };
-        extend: {
-            keyframes: {
-                ltr: {
-                    '0%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '49%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '98%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '99%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '100%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                };
-                rtl: {
-                    '0%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '49%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '98%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '99%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                    '100%': {
-                        transform: string;
-                        opacity: string;
-                    };
-                };
-                fadeOut: {
-                    '100%': {
-                        opacity: string;
-                    };
-                    '0%': {
-                        opacity: string;
-                    };
-                };
-                fadeIn: {
-                    '0%': {
-                        opacity: string;
-                    };
-                    '100%': {
-                        opacity: string;
-                    };
-                };
-            };
-            animation: {
-                lprompter: string;
-                rprompter: string;
-            };
-        };
+    fontSize: {
+      sm: string;
+      md: string;
+      base: string;
+      xl: string;
+      '1xl': string;
+      '2xl': string;
+      '3xl': string;
+      '4xl': string;
+      '5xl': string;
     };
-    plugins: any[];
-    corePlugins: {
-        preflight: false;
+    letterSpacing: {
+      a0: string;
+      a1: string;
+      a2: string;
+      a3: string;
+      a4: string;
+      a5: string;
+      a6: string;
+      b0: string;
+      b1: string;
+      b2: string;
+      b3: string;
+      b4: string;
+      b5: string;
+      b6: string;
     };
-    darkMode: ["variant", string];
+    lineHeight: {
+      a0: string;
+      a1: string;
+      a2: string;
+      a3: string;
+      a4: string;
+      a5: string;
+      a6: string;
+      b0: string;
+      b1: string;
+      b2: string;
+      b3: string;
+      b4: string;
+      b5: string;
+      b6: string;
+    };
+    spacing: {
+      0: string;
+      a0: string;
+      a1: string;
+      a2: string;
+      a3: string;
+      a4: string;
+      a5: string;
+      a6: string;
+      a7: string;
+      a8: string;
+      a9: string;
+      a10: string;
+      b0: string;
+      b1: string;
+      b2: string;
+      b3: string;
+      b4: string;
+      b5: string;
+      b6: string;
+    };
+    sizing: {
+      0: string;
+      a0: string;
+      a1: string;
+      a2: string;
+      a3: string;
+      a4: string;
+      a5: string;
+      a6: string;
+      a7: string;
+      a8: string;
+      a9: string;
+      a10: string;
+      b0: string;
+      b1: string;
+      b2: string;
+      b3: string;
+      b4: string;
+      b5: string;
+      b6: string;
+    };
+    aspectRatio: {
+      square: string;
+      blog: string;
+      tv: string;
+      cinema: string;
+    };
+    extend: {
+      keyframes: {
+        ltr: {
+          '0%': {
+            transform: string;
+            opacity: string;
+          };
+          '49%': {
+            transform: string;
+            opacity: string;
+          };
+          '98%': {
+            transform: string;
+            opacity: string;
+          };
+          '99%': {
+            transform: string;
+            opacity: string;
+          };
+          '100%': {
+            transform: string;
+            opacity: string;
+          };
+        };
+        rtl: {
+          '0%': {
+            transform: string;
+            opacity: string;
+          };
+          '49%': {
+            transform: string;
+            opacity: string;
+          };
+          '98%': {
+            transform: string;
+            opacity: string;
+          };
+          '99%': {
+            transform: string;
+            opacity: string;
+          };
+          '100%': {
+            transform: string;
+            opacity: string;
+          };
+        };
+        fadeOut: {
+          '100%': {
+            opacity: string;
+          };
+          '0%': {
+            opacity: string;
+          };
+        };
+        fadeIn: {
+          '0%': {
+            opacity: string;
+          };
+          '100%': {
+            opacity: string;
+          };
+        };
+      };
+      animation: {
+        lprompter: string;
+        rprompter: string;
+      };
+    };
+  };
+  plugins: any[];
+  corePlugins: {
+    preflight: false;
+  };
+  darkMode: ['variant', string];
 };
 export default _default;
--- src/molecules/06_Nav/index.ts
+++ src/molecules/06_Nav/index.ts
@@ -1,3 +1,9 @@
 // @atoms/Nav/index.ts
-export { default as Nav, ENavVariant, ENavControlVariant, ENavItemVariant, ESpotBadgeVariant } from './Nav';
+export {
+  default as Nav,
+  ENavVariant,
+  ENavControlVariant,
+  ENavItemVariant,
+  ESpotBadgeVariant,
+} from './Nav';
 export const NavName = 'Nav';
